120e3aee4d4b820a98f54ff6e1b6281c85659969
juvi <juvi@gmail.com>
2023-12-09 13:32:23 +0200

    user input to control pos

--- motor_drivers_test/motor_drivers_test.ino
+++ motor_drivers_test/motor_drivers_test.ino
@@ -4,15 +4,23 @@
 LinearActuator myactuator(BOCHNA1_HASAMA_CONTROL_PIN, BOCHNA1_HASAMA_SENSOR_PIN);
 
 void setup() {
- // Call the setup method on the actuator instance
- Serial.begin(9600);
- myactuator.setup();
- myactuator.setTarget(100,0,10000);
- Serial.printf("starting\n");
+  // Call the setup method on the actuator instance
+  Serial.begin(9600);
+  myactuator.setup();
+  myactuator.setTarget(100, 0, 10000);
+  Serial.printf("starting\n");
 }
 
+int nextpos = 0;
 void loop() {
- // Call the update method on the actuator instance
- myactuator.update();
- delay(10);
+  // Call the update method on the actuator instance
+  if (Serial.available()) {
+    nextpos = Serial.readStringUntil('\n').toInt();
+    Serial.printf("next pos:  %d\n", nextpos);
+    delay(100);
+    myactuator.setTarget(nextpos, 0, 10000);
+  }
+
+  myactuator.update();
+  delay(10);
 }
