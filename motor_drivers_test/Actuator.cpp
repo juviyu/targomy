13426e382dc8107ad23607c0d50fd5701bff05ba
juvi <juvi@gmail.com>
2023-12-09 13:31:41 +0200

    limiting speed from 1 to 1000 times a second

--- motor_drivers_test/Actuator.cpp
+++ motor_drivers_test/Actuator.cpp
@@ -1,29 +1,24 @@
 #include "Actuator.h"
 
-void Actuator::setTarget(int targetPos, int speed, unsigned long timeout)
-{
+void Actuator::setTarget(int targetPos, int speed, unsigned long timeout) {
   this->targetPos = targetPos;
-  stepDuration = 1000 / speed; // for when speed is in steps per seconds. can be multiplied by ticks per rotation to get rot/second
+  stepDuration = 1000 / constrain(speed, 1, 1000);  // for when speed is in steps per seconds. can be multiplied by ticks per rotation to get rot/second
 
   timeoutTime = millis() + timeout;
   onSetTarget();
   state = RUNNING;
 }
 
-void Actuator::update()
-{
-  if (millis() >= timeoutTime)
-  {
+void Actuator::update() {
+  if (millis() >= timeoutTime) {
+    Serial.printf("timout !\n");
     state = TIMEOUT;
     onTimeoutReached();
-  }
-  else
-  {
+  } else {
     onUpdate();
   }
 }
 
-ActuatorState Actuator::getState()
-{
+ActuatorState Actuator::getState() {
   return state;
 }
